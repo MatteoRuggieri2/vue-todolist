826ef8f008b3ec722dcfc0b808c60ff74048ad17
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-09 17:46:51 +0100

    Ora posso aggiungere le todo con il tasto enter

--- js/script.js
+++ js/script.js
@@ -13,11 +13,13 @@
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
 // MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato 
+// viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 // BONUS:
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente 
+//    (se done era uguale a false, impostare true e viceversa)
 
 
 
@@ -36,7 +38,7 @@ const app = new Vue(
 
                 {
                     text: 'andare dal barbiere',
-                    done: false
+                    done: true
                 },
 
                 {
